bd59b6f6ed950a90c916873aa9060191d2790cd2
SimonBdBc <simonbdbc@gmail.com>
2017-11-01 22:19:10 +0100

    add random

--- js/script.js
+++ js/script.js
@@ -36,15 +36,27 @@ $(document).ready(function() {
     };
     if(localStorage.getItem('goalPos') == null) {
       var pos = {
-        "x": JSON.parse(localStorage.getItem('gameAxes')).x,
-        "y": JSON.parse(localStorage.getItem('gameAxes')).y
+        "x": (Math.floor(((JSON.parse(localStorage.getItem('gameAxes')).x)-1)*Math.random())+1),
+        "y": (Math.floor(((JSON.parse(localStorage.getItem('gameAxes')).y)-1)*Math.random())+1)
       };
       localStorage.setItem('goalPos', JSON.stringify(pos));
     };
+    if(localStorage.getItem('mobPos') == null) {
+      var pos = {
+        "x": (Math.floor(((JSON.parse(localStorage.getItem('gameAxes')).x)-1)*Math.random())+1),
+        "y": (Math.floor(((JSON.parse(localStorage.getItem('gameAxes')).y)-1)*Math.random())+1)
+      };
+      localStorage.setItem('mobPos', JSON.stringify(pos));
+    };
+
     var playerPos = JSON.parse(localStorage.getItem('playerPos'));
     var goalPos = JSON.parse(localStorage.getItem('goalPos'));
+    var mobPos = JSON.parse(localStorage.getItem('mobPos'));
+
     $('.gameDiv[data-x="' + playerPos.x + '"][data-y="' + playerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
-    $('.gameDiv[data-x="' + goalPos.x + '"][data-y="' + goalPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/key.png">');
+    $('.gameDiv[data-x="' + goalPos.x + '"][data-y="' + goalPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/door.jpeg">');
+    $('.gameDiv[data-x="' + mobPos.x + '"][data-y="' + mobPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/mob.png">');
+
     $('body').on('keydown', function(e) {
       if(e.keyCode == 38 /* up */) {
         movePlayer('UP');
@@ -55,28 +67,44 @@ $(document).ready(function() {
       } else if(e.keyCode == 39 /* right */) {
         movePlayer('RIGHT');
       }
-
     });
   }
-function checkVictory(currentPlayerPos) {
-  if(currentPlayerPos.x == JSON.parse(localStorage.getItem('goalPos')).x && currentPlayerPos.y == JSON.parse(localStorage.getItem('goalPos')).y) {
-    displayScreen('victory');
-    localStorage.setItem('gameState', 'victory');
+  function checkVictory(currentPlayerPos) {
+    if(currentPlayerPos.x == JSON.parse(localStorage.getItem('goalPos')).x && currentPlayerPos.y == JSON.parse(localStorage.getItem('goalPos')).y) {
+  
+
+
+
+
+    }
   }
-}
+  function checkLoose(currentPlayerPos) {
+    if(currentPlayerPos.x == JSON.parse(localStorage.getItem('mobPos')).x && currentPlayerPos.y == JSON.parse(localStorage.getItem('mobPos')).y) {
+      displayScreen('loose');
+      localStorage.setItem('gameState', 'loose');
+    }
+  }
 
   function movePlayer(direction) {
     var currentPlayerPos = JSON.parse(localStorage.getItem('playerPos'));
     var gameSize = JSON.parse(localStorage.getItem('gameAxes'));
+    var audioBumpWall = new Audio('../sound/invalid.mp3');
     if(direction == "UP") {
       if((currentPlayerPos.y - 1) > 0){
         $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('');
         currentPlayerPos.y -= 1;
         $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
         checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
         localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       } else {
-        console.log('aie !');
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('');
+        currentPlayerPos.y += 1;
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
+        audioBumpWall.play();
+        checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
+        localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       }
     } else if(direction == "DOWN") {
       if((currentPlayerPos.y + 1) <= gameSize.y){
@@ -84,9 +112,16 @@ function checkVictory(currentPlayerPos) {
         currentPlayerPos.y += 1;
         $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
         checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
         localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       } else {
-        console.log('aie !');
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('');
+        currentPlayerPos.y -= 1;
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
+        audioBumpWall.play();
+        checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
+        localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       }
     } else if(direction == "LEFT") {
       if((currentPlayerPos.x - 1) > 0){
@@ -94,9 +129,16 @@ function checkVictory(currentPlayerPos) {
         currentPlayerPos.x -= 1;
         $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
         checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
         localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       } else {
-        console.log('aie !');
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('');
+        currentPlayerPos.x += 1;
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
+        audioBumpWall.play();
+        checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
+        localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       }
     } else if(direction == "RIGHT") {
       if((currentPlayerPos.x + 1) <= gameSize.x){
@@ -104,38 +146,45 @@ function checkVictory(currentPlayerPos) {
         currentPlayerPos.x += 1;
         $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
         checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
         localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       } else {
-        console.log('aie !');
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('');
+        currentPlayerPos.x -= 1;
+        $('.gameDiv[data-x="' + currentPlayerPos.x + '"][data-y="' + currentPlayerPos.y + '"]').html('<img style="width:100%; height:100%" src="../image/link.png">');
+        audioBumpWall.play();
+        checkVictory(currentPlayerPos);
+        checkLoose(currentPlayerPos);
+        localStorage.setItem('playerPos', JSON.stringify(currentPlayerPos));
       }
     }
   }
   function displayScreen(gameState) {
-  $.each($('section[data-state!="' + gameState + '"]'), function(key, value) {
-    $(this).addClass('hidden');
-  });
-  $('section[data-state="' + gameState + '"]').removeClass('hidden');
-};
-$('button[data-action="startGame"]').on('click', function() {
-  var baseX = $('input[name="x"]').val();
-  var baseY = $('input[name="y"]').val();
-  if(baseX < "1" || baseY < "1") {
-    alert('erreur de valeur X ou Y');
-  } else {
-    var axes = {
-      "x": baseX,
-      "y": baseY
-    };
-    localStorage.setItem('gameAxes', JSON.stringify(axes));
-    localStorage.setItem('gameState', "playScreen");
-    generateGame();
-    displayScreen(localStorage.getItem('gameState'));
+    $.each($('section[data-state!="' + gameState + '"]'), function(key, value) {
+      $(this).addClass('hidden');
+    });
+    $('section[data-state="' + gameState + '"]').removeClass('hidden');
   };
-});
-$('button[data-action="reset"]').on('click', function() {
-  localStorage.clear();
-  location.reload();
-});
+  $('button[data-action="startGame"]').on('click', function() {
+    var baseX = $('input[name="x"]').val();
+    var baseY = $('input[name="y"]').val();
+    if(baseX < "1" || baseY < "1") {
+      alert('erreur de valeur X ou Y');
+    } else {
+      var axes = {
+        "x": baseX,
+        "y": baseY
+      };
+      localStorage.setItem('gameAxes', JSON.stringify(axes));
+      localStorage.setItem('gameState', "playScreen");
+      generateGame();
+      displayScreen(localStorage.getItem('gameState'));
+    };
+  });
+  $('button[data-action="reset"]').on('click', function() {
+    localStorage.clear();
+    location.reload();
+  });
 
 
   init();
